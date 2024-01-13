35975285941b71786ee6ae6962ab4a8a44ca9d8f
Koks-creator <73878161+Koks-creator@users.noreply.github.com>
2024-01-13 19:16:15 +0100

    Update CustomLogger.py

--- CustomLogger.py
+++ CustomLogger.py
@@ -7,17 +7,17 @@ class CustomLogger:
     format: str = "%(asctime)s - %(name)s - %(levelname)s - Line: %(lineno)s - %(message)s"
     file_handler_format: logging.Formatter = logging.Formatter(format)
     log_file_name: str = "logs.txt"
-    logger_name = __name__
+    logger_name: str = __name__
     logger_log_level: int = logging.ERROR
     file_handler_log_level: int = logging.ERROR
 
     def create_logger(self) -> logging.Logger:
         logging.basicConfig(format=self.format)
         logger = logging.getLogger(self.logger_name)
-        logger.setLevel(logging.ERROR)
+        logger.setLevel(self.logger_log_level)
 
         file_handler = logging.FileHandler(self.log_file_name)
-        file_handler.setLevel(logging.ERROR)
+        file_handler.setLevel(self.logger_log_level)
 
         file_handler.setFormatter(self.file_handler_format)
         logger.addHandler(file_handler)
